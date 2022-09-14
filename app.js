44f3181886b5e1deb7141e375b845820f222db28
julien <julien.pardoel@gmail.com>
2022-09-15 01:12:06 +0200

    affichage tete

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const gameSize = 600;
 const squareSize = 20;
 const snake = new Snake(squareSize);
+const canvas = document.querySelector("#game");
+const ctx = canvas.getContext("2d");
 
 function update() {
+  snake.update();
   setTimeout(update, 300);
 }
 function start() {
--- snake.js
+++ snake.js
@@ -2,7 +2,18 @@ class Snake {
   constructor(size) {
     this.x = 0;
     this.y = 0;
-    this.size = size;
-    console.log("tête créée");
+    this.blockSize = size;
+    this.blocks = [];
+    this.addBlock(this.x, this.y);
+    console.log(this.blocks);
+  }
+  addBlock(x, y) {
+    const block = new Block(x, y, this.blockSize);
+    this.blocks.push(block);
+  }
+  update() {
+    for (const block of this.blocks) {
+      block.draw();
+    }
   }
 }
